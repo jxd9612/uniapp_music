7c1e15cf8e9385e84cc4ddeb1619a1a8cf2e94d7
jxd9612 <15916537217@163.com>
2019-10-21 17:55:19 +0800

    轻修改

--- App.vue
+++ App.vue
@@ -8,6 +8,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import 'components/colorui/main.css';
+@import 'components/colorui/icon.css';
 /* uni-app 引入本地iconfont的正确姿势：https://ask.dcloud.net.cn/article/35443 */
 @import 'static/fonts/iconfont.css';
 page {
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -15,16 +15,86 @@
 			<view class="tab-find">发现</view>
 			<view class="tab-video">视频</view>
 		</view>
+		<!-- banner -->
+		<swiper class="card-swiper" :circular="true" :autoplay="true" @change="cardSwiper">
+			<swiper-item v-for="(item, index) in bannerList" :key="index" :class="cardCur == index ? 'cur' : ''">
+				<view class="swiper-item"><image :src="item.imageUrl" mode="aspectFit"></image></view>
+			</swiper-item>
+		</swiper>
+		<!-- 菜单 -->
+		<view class="menu">
+			<!-- 菜单导航列表 -->
+			<view class="menu-nav">
+				<view class="menu-nav-item">
+					<image src="../../static/images/meirituijian.png"></image>
+					<text>每日推荐</text>
+				</view>
+				<view class="menu-nav-item">
+					<image src="../../static/images/musicList.png"></image>
+					<text>音乐列表</text>
+				</view>
+				<view class="menu-nav-item">
+					<image src="../../static/images/rankingList.png"></image>
+					<text>排行榜</text>
+				</view>
+				<view class="menu-nav-item">
+					<image src="../../static/images/musicRadio.png"></image>
+					<text>音乐电台</text>
+				</view>
+				<view class="menu-nav-item">
+					<image src="../../static/images/musicVedeo.png"></image>
+					<text>音乐视频</text>
+				</view>
+			</view>
+			<!-- 音乐风格 -->
+			<view class="menu-style"></view>
+			<!-- 新唱片 -->
+			<view class="menu-record"></view>
+		</view>
 	</view>
 </template>
 
 <script>
+import { getBanner } from '../../api/mine.js';
+
 export default {
 	data() {
-		return {};
+		return {
+			cardCur: 0,
+			bannerList: []
+		};
 	},
-	onLoad() {},
-	methods: {}
+	onLoad() {
+		this.init();
+	},
+	methods: {
+		init() {
+			this.handleBanner();
+		},
+		async handleBanner() {
+			// 0-pc、1-android、2-iphone、3-ipad
+			let reqData = {};
+			switch (uni.getSystemInfoSync().platform) {
+				case 'android':
+					reqData.type = 1;
+					break;
+				case 'ios':
+					reqData.type = 2;
+					break;
+				default:
+					reqData.type = 0;
+					break;
+			}
+			let data = await getBanner(reqData);
+			console.log(data);
+			if (data) {
+				this.bannerList = data;
+			}
+		},
+		cardSwiper(e) {
+			this.cardCur = e.detail.current;
+		}
+	}
 };
 </script>
 
@@ -74,6 +144,31 @@ export default {
 		.tab-video {
 		}
 		.active {
+		}
+	}
+	.menu {
+		width: 700upx;
+		margin-left: 25upx;
+		.menu-nav {
+			width: 100%;
+			display: flex;
+			justify-content: space-between;
+			padding-bottom: 30upx;
+			border-bottom: 2upx solid $border-base;
+			.menu-nav-item {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				text-align: center;
+				image {
+					width: 100upx;
+					height: 100upx;
+				}
+				text {
+					margin-top: 15upx;
+					font-size: $text-base;
+				}
+			}
 		}
 	}
 }
