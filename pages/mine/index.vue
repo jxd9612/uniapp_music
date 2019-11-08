1fe8dc471d3ca3a3677e23f80eeff64ca7baddf0
jxd9612 <15916537217@163.com>
2019-11-08 17:51:36 +0800

    增加我的/视频 模块

---
+++ pages/mine/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="container">我的页面</view>
+</template>
+
+<script>
+export default {
+	name: 'mine'
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/video/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="container">视频页面</view>
+</template>
+
+<script>
+export default {
+	name: 'videos'
+};
+</script>
+
+<style lang="scss" scoped></style>
